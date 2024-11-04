086675ca6b6194198c315e1cedeb5cabe055a658
xhulio11 <xhuliano1998@gmail.com>
2024-11-04 20:22:33 +0200

    Delted Files

--- src/main6.py
+++
@@ -1,3 +0,0 @@
-from google_news_api import GNews 
-
-google_news = GNews(topic='Cars')
--- src/try2.py
+++
@@ -1,7 +0,0 @@
-import json 
-
-with open('artilces.json', 'r') as file: 
-    data = json.load(file)
-
-
-print(len(data))

cbc0f7adfcd227bc9e10f403efbabba08e94c84e
xhulio11 <xhuliano1998@gmail.com>
2025-06-07 19:22:54 +0300

    test.py file contains some demo soultions to problems.

---
+++ test.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import SessionNotCreatedException
+import time 
+import subprocess 
+import sys
+import os
+import json 
+# Add the absolute path of the src directory to sys.path
+sys.path.append(os.path.abspath("/home/xhulio/Desktop/GoNews/src"))
+from google_news_api import *
+
+
+def get_chrome_driver():
+    # Set up Selenium options
+    chrome_options = Options()
+    service = Service('/usr/bin/chromedriver') 
+    chrome_options.add_argument('--disable-notifications')
+    # If you have a specific user-data and profile:
+    chrome_options.add_argument("--user-data-dir=/home/xhulio/.config/chromium/")
+    chrome_options.add_argument("--profile-directory=Profile 2")
+
+    return webdriver.Chrome(service=service, options=chrome_options)
+
+def kill_chrome_processes():
+    # Attempts to kill all Chromium/Chrome processes.
+    # If your system uses `chromium-browser`, adjust accordingly.
+    subprocess.run(["pkill", "-9", "chromium"], check=False)
+    subprocess.run(["pkill", "-9", "chrome"], check=False)
+
+try:
+    driver = get_chrome_driver()
+
+except SessionNotCreatedException as e:
+    print("SessionNotCreatedException encountered. Attempting to kill Chrome/Chromium and retry...")
+    kill_chrome_processes()
+    time.sleep(1)  # small delay to ensure processes are fully shut down
+    # Attempt to re-initialize
+    driver = get_chrome_driver()
+except Exception as e:  
+    print("Some undefined error occured")
+    exit()
+
+
+google_api = GoNews(language='english', country='United States')
+
+print("Receiving urls")
+url_news = google_api.get_news_by_topic(topic='POLITICS')
+print("EXIT")
+
+with open("urls.json", "w", encoding="UTF-8") as file: 
+    json.dump(url_news, file,indent=4)
+
+print("Receiving Content")
+content = google_api.read_articles(url_news, driver, write_json=True, max_topics=5)
